62b17e7e9ef85998dbbff10d2b7014a317b05c86
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2022-03-22 20:03:39 +0100

    Delete doctor

---
+++ app/Controllers/Admin/AdminDoctorsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Services.Admin;
+using backend.Helpers;
+
+namespace backend.Controllers.Admin
+{
+    [Route("admin/doctors")]
+    [ApiController]
+    public class AdminDoctorsController : ControllerBase
+    {
+        private readonly AdminDoctorsService doctorsService;
+
+        public AdminDoctorsController(AdminDoctorsService doctorsService)
+        {
+            this.doctorsService = doctorsService;
+        }
+
+        // DELETE admin/doctors/:doctor-id
+        [HttpDelete("{doctorId:int}")]
+        [Authorize(AccountTypeEnum.Admin)]
+        public async Task<IActionResult> DeleteDoctor(int doctorId)
+        {
+            return Ok(await this.doctorsService.DeleteDoctor(doctorId));
+        }
+    }
+}
---
+++ app/Services/Admin/AdminDoctorsService.cs
@@ -0,0 +1,29 @@
+﻿using backend.Database;
+using backend.Dto.Responses;
+using backend.Exceptions;
+
+namespace backend.Services.Admin
+{
+    public class AdminDoctorsService
+    {
+        private readonly DataContext dataContext;
+
+        public AdminDoctorsService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<SuccessResponse> DeleteDoctor(int doctorId)
+        {
+            var doctor = this.dataContext.Doctors.FirstOrDefault(doctor => doctor.Id == doctorId);
+
+            if (doctor == null)
+                throw new NotFoundException();
+
+            this.dataContext.Doctors.Remove(doctor);
+            this.dataContext.SaveChanges();
+
+            return new SuccessResponse();
+        }
+    }
+}
