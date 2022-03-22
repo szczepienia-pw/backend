390e209e5c46e5ab01806d9e6810f4b468f97fc9
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-03-22 16:23:44 +0100

    Add test for showing vaccination slots with filtering

--- tests/Unit/Services/Doctor/VaccinationSlotServiceTest.cs
+++ tests/Unit/Services/Doctor/VaccinationSlotServiceTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using backend.Dto.Requests.Doctor.VaccinationSlot;
 using backend.Dto.Responses;
+using backend.Dto.Responses.Doctor.VaccinationSlot;
 using backend.Exceptions;
 using backend.Models.Visits;
 using backend.Services.Doctor;
@@ -126,6 +127,109 @@ namespace backend_tests.Unit.Services.Doctor
             await Assert.ThrowsAsync<NotFoundException>(
                 () => vaccinationService.DeleteSlot(4, firstDoctor)
             );
+        }
+
+        [Theory]
+        // Without start date
+        [InlineData(null, null, null, new int[]{ 1, 2, 3, 4, 5, 6 })]
+        [InlineData(1, null, null, new int[]{ 2, 4, 6 })]
+        [InlineData(0, null, null, new int[]{ 1, 3, 5 })]
+        // With start date
+        [InlineData(null, "2022-04-02T14:00:00Z", null, new int[]{ 3, 4, 5, 6 })]
+        [InlineData(1, "2022-04-02T14:00:00Z", null, new int[] { 4, 6 })]
+        [InlineData(0, "2022-04-02T14:00:00Z", null, new int[] { 3, 5 })]
+        // With end date
+        [InlineData(null, null, "2022-04-19T14:00:00Z", new int[] { 1, 2, 3, 4 })]
+        [InlineData(1, null, "2022-04-19T14:00:00Z", new int[] { 2, 4 })]
+        [InlineData(0, null, "2022-04-19T14:00:00Z", new int[] { 1, 3 })]
+        // With start and end date
+        [InlineData(null, "2022-04-02T14:00:00Z", "2022-04-19T14:00:00Z", new int[] { 3, 4 })]
+        [InlineData(1, "2022-04-02T14:00:00Z", "2022-04-19T14:00:00Z", new int[] { 4 })]
+        [InlineData(0, "2022-04-02T14:00:00Z", "2022-04-19T14:00:00Z", new int[] { 3 })]
+        // Edge cases
+        [InlineData(null, "2022-04-20T14:00:01Z", null, new int[] {})]
+        [InlineData(null, null, "2022-04-01T13:59:59Z", new int[] { })]
+        public async Task TestShouldCorrectlyFilterVaccinationSlots(int? onlyReserved, string? startDate, string? endDate, int[] shownIds)
+        {
+            var dataContext = DbHelper.GetMockedDataContextWithAccounts();
+            var firstDoctor = dataContext.Object.Doctors.First(doctor => doctor.Id == 1);
+            var secondDoctor = dataContext.Object.Doctors.First(doctor => doctor.Id == 2);
+
+            var vaccinationSlots = new List<VaccinationSlotModel>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Date = DateTime.Parse("2022-04-01T14:00:00Z"),
+                    Reserved = false,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 2,
+                    Date = DateTime.Parse("2022-04-01T14:00:00Z"),
+                    Reserved = true,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 3,
+                    Date = DateTime.Parse("2022-04-10T14:00:00Z"),
+                    Reserved = false,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 4,
+                    Date = DateTime.Parse("2022-04-10T14:00:00Z"),
+                    Reserved = true,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 5,
+                    Date = DateTime.Parse("2022-04-20T14:00:00Z"),
+                    Reserved = false,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 6,
+                    Date = DateTime.Parse("2022-04-20T14:00:00Z"),
+                    Reserved = true,
+                    Doctor = firstDoctor
+                },
+                new()
+                {
+                    Id = 7,
+                    Date = DateTime.Parse("2022-04-01T14:00:00Z"),
+                    Reserved = false,
+                    Doctor = secondDoctor
+                },
+                new()
+                {
+                    Id = 8,
+                    Date = DateTime.Parse("2022-04-01T14:00:00Z"),
+                    Reserved = true,
+                    Doctor = secondDoctor
+                }
+            };
+
+            dataContext.Setup(context => context.VaccinationSlots).Returns(vaccinationSlots.AsQueryable().BuildMockDbSet().Object);
+            var vaccinationService = new VaccinationSlotService(dataContext.Object);
+
+            var request = new FilterVaccinationSlotsRequest()
+            {
+                OnlyReserved = onlyReserved,
+                StartDate = startDate,
+                EndDate = endDate
+            };
+            var result = await vaccinationService.GetSlots(request, firstDoctor);
+
+            Assert.IsType<PaginatedResponse<VaccinationSlotModel, List<VaccinationSlotResponse>>>(result);
+
+            var vaccinationSlotIds = result.Data.Select(vaccinationSlotResponse => vaccinationSlotResponse.Id).ToArray();
+            Assert.Equal(shownIds, vaccinationSlotIds);
         }
     }
 }
