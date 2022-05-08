2a7e8f72b0113b95c2a7680399bba89c6b96cd7b
Tymoteusz Bartnik <tymoteusz.bartnik@machineportal.com>
2022-05-08 16:41:20 +0200

    Revert "Revert "Testy""
    This reverts commit b1000bf278fed7c5dda2dcaa4f0d205362d2ed53.

--- app/Dto/Responses/Patient/Vaccination/VaccinationResponse.cs
+++ app/Dto/Responses/Patient/Vaccination/VaccinationResponse.cs
@@ -1,4 +1,5 @@
-﻿using backend.Dto.Responses.Common.Vaccination;
+﻿using System.Diagnostics;
+using backend.Dto.Responses.Common.Vaccination;
 using backend.Models.Accounts;
 using backend.Models.Visits;
 
@@ -17,7 +18,7 @@ namespace backend.Dto.Responses.Patient.Vaccination
 		{
 			this.Id = vaccination.Id;
 			this.Vaccine = new VaccineResponse(vaccination.Vaccine);
-            this.VaccinationSlot = new AvailableSlotResponse(vaccination.VaccinationSlot);
+			this.VaccinationSlot = new AvailableSlotResponse(vaccination.VaccinationSlot);
 			this.Status = vaccination.Status.ToString();
 			this.Patient = vaccination.Patient;
             this.Doctor = vaccination.Doctor;
